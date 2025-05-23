8d7b029f5593f15a13e96da43402e6c4d0b4c6bb
zuhair khan <zuhairkhan5134@gmail.com>
2025-05-23 16:06:08 +0530

    done

--- src/db.js
+++ src/db.js
@@ -6,7 +6,7 @@ const connectDB = async () => {
         logger.info(`MongoDB Connected: ${conn.connection.host}`)
         console.log(`MongoDB Connected: ${conn.connection.host}`)
     } catch (error) {
-        console.error(`Error: ${error.message}`.red.underline.bold)
+        console.error(`Error: ${error.message}`)
         process.exit(1)
     }
 }
